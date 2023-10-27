c25f6296534a12ba9ca3ec0b83f64abb5deea533
shuvo-cefalo <mrityunjoy.das@cefalo.com>
2023-10-27 12:12:51 +0600

    - Simple CRUD operation without DB - Repository pattern with Dependency Injection

---
+++ SuperHeroApi/Controllers/WeatherForcastController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SuperHeroApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForcastController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<WeatherForcastController> _logger;
+
+    public WeatherForcastController(ILogger<WeatherForcastController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public IEnumerable<WeatherForecast> Get()
+    {
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
+    }
+}
+
---
+++ SuperHeroApi/Services/ISuperHeroService.cs
@@ -0,0 +1,10 @@
+namespace SuperHeroApi.Services;
+
+public interface ISuperHeroService
+{
+    List<SuperHero> GetAllHeroes();
+    SuperHero GetSingleHero(int id);
+    List<SuperHero>? AddHero(SuperHero hero);
+    List<SuperHero>? UpdateHero(int id, SuperHero request);
+    List<SuperHero>? DeleteHero(int id);
+}
